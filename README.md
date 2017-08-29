88f4dead9255dc428d9b7d1c36913b655dd591bc
bdynamite <bdynamite@yandex.ru>
2017-08-29 18:39:08 +0300

    исправлены ошибки js

---
+++ README.md
@@ -0,0 +1,7 @@
+# Site for the Mood
+
+Site with jokes is [here](https://bdynamite.github.io/20_mood/)
+
+# Project Goals
+
+The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function get_joke() {
+function insert_joke() {
   var arr = [
   {
     'phrase': "Если ты будешь усердно работать по 8 часов в день, то обязательно станешь боссом и начнешь работать по 12"
@@ -19,10 +19,13 @@ function get_joke() {
     'phrase': "Хорошая жизнь. Жить я её, конечно, не буду."
   },
   {
+    'phrase': "Вывеска над кафе — '&lt;Мы открылись&gt;'. Что-то не так. Где же '&lt;/Мы открылись&gt;'?"
+  },
+  {
     'phrase': "Взросление — это что-то про квартплату"
   }
   ];
   var rand = Math.floor(Math.random() * arr.length);
   var elem = document.getElementById("joke")
-  elem.innerHTML = "<p>" + arr[rand].phrase + "</p>";
+  elem.innerHTML = arr[rand].phrase;
 }
